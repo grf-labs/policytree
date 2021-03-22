e861f4d91096975497562dee442e7d601c45a398
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-03-22 15:05:23 -0700

    expect_warning

--- r-package/policytree/tests/testthat/test_multi_causal_forest.R
+++ r-package/policytree/tests/testthat/test_multi_causal_forest.R
@@ -7,7 +7,7 @@ test_that("everything runs", {
   W <- sample(1:d, n, replace = TRUE)
 
   # Some simple smoke tests
-  mcf <- multi_causal_forest(X, Y, W)
+  expect_warning(mcf <- multi_causal_forest(X, Y, W))
   predict(mcf)
   conditional_means(mcf)
   double_robust_scores(mcf)
@@ -41,7 +41,7 @@ test_that("predictions have not changed", {
   X <- matrix(runif(n * p), n, p)
   Y <- runif(n)
   W <- sample(1:d, n, replace = TRUE)
-  mcf <- multi_causal_forest(X = X, Y = Y, W = W, seed = 123)
+  expect_warning(mcf <- multi_causal_forest(X = X, Y = Y, W = W, seed = 123))
   tau <- predict(mcf)$predictions
 
   expect_equal(mean(colMeans(tau)), -0.001, tolerance = 1e-3)
