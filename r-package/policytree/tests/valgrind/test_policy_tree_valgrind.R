355ded8c20539e34aa6ced3bebd350a99a518522
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-04-30 16:34:21 -0700

    add a valgrind check

---
+++ r-package/policytree/tests/valgrind/test_policy_tree_valgrind.R
@@ -0,0 +1,18 @@
+# Check that the following tree search code path passes under valgrind.
+# Usage:
+# R -d "valgrind --tool=memcheck --leak-check=full" --vanilla  < test_policy_tree_valgrind.R
+library(policytree)
+depth <- 3
+n <- 50
+p <- 5
+d <- 4
+# 1/2 continuous/discrete X
+X.c <- matrix(rnorm(n * p), n, p)
+X.d <- matrix(sample(10:20, n * p, replace = TRUE), n, p)
+X <- cbind(X.c, X.d)
+Y <- matrix(rnorm(n * d), n, d)
+
+tree <- policy_tree(X, Y, depth = depth)
+pp <- predict(tree, X)
+tree.step <- policy_tree(X, Y, depth = depth, split.step = 2)
+pp.step <- predict(tree.step, X)
